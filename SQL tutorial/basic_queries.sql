ab5cc81300d203f7a388ef57303f25fa7e56d409
Durga Prasad <131277100+Durga1706@users.noreply.github.com>
2023-10-21 14:05:19 +0530

    Update basic_queries.sql

--- SQL tutorial/basic_queries.sql
+++ SQL tutorial/basic_queries.sql
@@ -1,8 +1,8 @@
-// https://www.youtube.com/watch?v=v8Ec1MsbNig
-// IFF
+/*https://www.youtube.com/watch?v=v8Ec1MsbNig */
+-- IFF
 select e_id,e_name iif(e_age>30,"Old employee","young employee") as employee_generation from employee;
 
-//Scalar valued function
+--Scalar valued function
 create function add_five(@num as int) RETURNS int AS BEGIN RETURN(@num+5) end;
 select dob.add_five(100)
 

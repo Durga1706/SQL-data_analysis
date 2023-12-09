9eb585af1d730a679945398bba0f781e3c92bcc8
Durga Prasad <131277100+Durga1706@users.noreply.github.com>
2023-12-09 17:00:45 +0530

    Create filtering.sql

---
+++ SQL tutorial/filtering.sql
@@ -0,0 +1,8 @@
+/*  * Basics of Filtering with SQL
+     https://d3c33hcgiwev3.cloudfront.net/_31f3d7f7e2769ddc96591a0d92915013_Basics-of-Filtering-with-SQL.pdf?Expires=1702252800&Signature=O7SghSOj3f8hEsMAupL19fxWikBLtPHh
+     FgxdThDVNXsmNj-g7EdoIFvDlUM~qurWPXOEVaIQgzL10XiTMHW0XMcVA33HRashlOSrMPTjhzQFNLVWkZ5SlWIUSDgYE2da9ytsyefiWCwLyM8BYfKTlnbUYsqyNUDyE~bDfbF65BY_&Key-Pair-Id=APKAJLTNE6QMUY6HBC5A
+
+    * Advanced Filtering: IN, OR, and NOT
+    https://d3c33hcgiwev3.cloudfront.net/_034d905fed357262bbc44ea0ccfe7178_Advanced-Filtering-IN-OR-NOT.pdf?Expires=1702252800&Signature=SwrPeVu0N6ZkmovvZjBHLcd
+    MTHHTAuGq2DoQBWS2XxdJeYQLZQ3033ZYqjWfTBX5~kZtOw9yRwWBbaxZNPWS~j4Rhfp28erLGC2rse8zocbVfIdslVJjgj3eW2iLiPEYklJAI0DiVtwQJPWccWY-UKVdh7ftTQPAg13fA6FqLd8_&Key-Pair-Id=APKAJLTNE6QMUY6HBC5A
+*/

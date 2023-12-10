7629beea43e4d86dd520b44905b544b3a965cb3d
Durga Prasad <131277100+Durga1706@users.noreply.github.com>
2023-12-10 13:53:06 +0530

    Update and rename filtering.sql to filtering_sorting.sql

--- SQL tutorial/filtering_sorting.sql
+++ SQL tutorial/filtering_sorting.sql
@@ -5,4 +5,10 @@
     * Advanced Filtering: IN, OR, and NOT
     https://d3c33hcgiwev3.cloudfront.net/_034d905fed357262bbc44ea0ccfe7178_Advanced-Filtering-IN-OR-NOT.pdf?Expires=1702252800&Signature=SwrPeVu0N6ZkmovvZjBHLcd
     MTHHTAuGq2DoQBWS2XxdJeYQLZQ3033ZYqjWfTBX5~kZtOw9yRwWBbaxZNPWS~j4Rhfp28erLGC2rse8zocbVfIdslVJjgj3eW2iLiPEYklJAI0DiVtwQJPWccWY-UKVdh7ftTQPAg13fA6FqLd8_&Key-Pair-Id=APKAJLTNE6QMUY6HBC5A
+
+    * wildcards
+    https://d3c33hcgiwev3.cloudfront.net/_5b62ac24aa1b72bfef6a44c0d0eafb09_Using-Wildcards-in-SQL.pdf?Expires=1702339200&Signature=ULWe0pDnUJXfq7Lh~Z0HhhGEigWFUVK-
+    hLghs25RPdBXar02Oe4mYkc3OnnxHw2qQUGirUEEY72l-CO3t1-TwOO5xwkz-QI9JP20E5msf87Mml9CpGZx7fucPK2IqqdmbdLJDp1HoRC6hQyMn5kOmGsylCb~jNW0yKKxdhK6I8k_&Key-Pair-Id=APKAJLTNE6QMUY6HBC5A
+
+    *
 */

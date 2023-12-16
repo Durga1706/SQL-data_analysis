63de943988bd324ce41a08d57c4cf5c4bb1c1831
Durga Prasad <131277100+Durga1706@users.noreply.github.com>
2023-12-16 17:46:50 +0530

    Update filtering_sorting.sql

--- SQL tutorial/filtering_sorting.sql
+++ SQL tutorial/filtering_sorting.sql
@@ -19,10 +19,30 @@
      * 
 */
 
--- GROPU BY
+-- GROPU BY: aggregate all rows by a specific col
+CREATE TABLE transactions (
+    transaction_id INT PRIMARY KEY AUTO_INCREMENT,
+    amount DECIMAL(5, 2),
+    customer_id INT,
+    order_date DATE
+);
 
+INSERT INTO transactions VALUES  (1000, 4.99, 3, "2023-01-01"), (1001, 2.89, 2, "2023-01-01"), (1002, 3.38, 3, "2023-01-02"),
+(1003, 4.99, 1, "2023-01-02"), (1004, 1.00, NULL, "2023-01-03"), (1005, 2.49, 4, "2023-01-03"), (1006, 5.48, NULL, "2023-01-03");
+        
+SELECT * FROM transactions;
+-- tot amount on each day 
+select sum(amount), order_date from transactions group by order_date;
+
+select sum(amount), customer_id from transactions group by customer_id;
 
 -- HAVING
+-- WHERE clause doesn't work in group by
+-- select count(amount), order_date from transactions group by order_date where count(amount)>1;
+
+-- in that place use HAVING
+select count(amount), order_date from transactions group by order_date having count(amount)>1;
+
 
 -- ORDER BY
 CREATE TABLE employee( 
